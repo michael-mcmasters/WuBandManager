628c350f86bc8eb1c89d24a20765fb52f8551217
michael-mcmasters <jmcmasters411@gmail.com>
2020-11-02 20:39:27 -0500

    Complete all loops and pass all tests

--- src/main/java/com/codedifferently/BandManager.java
+++ src/main/java/com/codedifferently/BandManager.java
@@ -10,6 +10,19 @@ public class BandManager {
 
     public String whileLoop() {
         String result = "";
+
+        int index = 0;
+        while(index < wuMemberArray.length) {
+            result += "\n";
+            result += "My first name is " + wuMemberArray[index].getFirstName() + "\n";
+            result += "My last name is " + wuMemberArray[index].getLastName() + "\n";
+            result += "My stage name is " + wuMemberArray[index].getStageName();
+
+            index++;
+        }
+
+        return result;
+
         // create a `counter`
         // while `counter` is less than length of array
             // begin loop
@@ -19,13 +32,24 @@ public class BandManager {
                 // append `stringRepresentation` to `result` variable
 
             // end loop
-        return result;
+
+
+
+        //return result;
     }
 
 
 
     public String forLoop() {
+
         String result = "";
+        for (int index = 0; index < wuMemberArray.length; index++) {
+            result += "\n";
+            result += "My first name is " + wuMemberArray[index].getFirstName() + "\n";
+            result += "My last name is " + wuMemberArray[index].getLastName() + "\n";
+            result += "My stage name is " + wuMemberArray[index].getStageName();
+        }
+
         // identify initial value
         // identify terminal condition
         // identify increment
@@ -44,6 +68,14 @@ public class BandManager {
 
     public String forEachLoop() {
         String result = "";
+
+        for (WuMember member : wuMemberArray) {
+            result += "\n";
+            result += "My first name is " + member.getFirstName() + "\n";
+            result += "My last name is " + member.getLastName() + "\n";
+            result += "My stage name is " + member.getStageName();
+        }
+
         // identify array's type
         // identify array's variable-name
 
